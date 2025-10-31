e4abb8cdb311d9503f019dc650c19056c0af194e
Aziz Shoimqulov <azizshoimqulov74@gmail.com>
2025-10-31 11:50:27 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import Started from './components/started.vue'
+</script>
+
+<template>
+  <Started />
+</template>
---
+++ src/components/started.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Hello World</h1>
+</template>
+
+<style scoped>
+h1 {
+  color: blue;
+}
+</style>
