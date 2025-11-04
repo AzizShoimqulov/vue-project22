3dc752d6ab8cd844d953c804ad7a14cf4e6ecf6b
Aziz Shoimqulov <azizshoimqulov74@gmail.com>
2025-11-04 13:20:30 +0500

    dfasfd

--- src/components/Search-panel.vue
+++ src/components/Search-panel.vue
@@ -1,17 +1,16 @@
 <template>
     <div>
-        
+        <h1>Search panel</h1>
     </div>
 </template>
 
 
 <script>
-export default {
-    
-}
+export default {}
+
 </script>
 
 
 
-<style>    
+<style scoped>    
 </style>
