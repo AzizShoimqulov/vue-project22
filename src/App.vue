439064b81b19bb45f7e29fde2bf4385a93b668c1
Aziz Shoimqulov <azizshoimqulov74@gmail.com>
2025-11-04 11:22:34 +0500

    dsada

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,9 @@ import Started from './components/started.vue'
   <div class="app">
     <div class="content">
       <Started/>
-      <div class="search-panel"></div>
+      <div class="search-panel">
+        
+      </div>
     </div>
   </div>
 </template>
@@ -22,5 +24,12 @@ import Started from './components/started.vue'
     background-color: #fff;
     margin: 0 auto;
     padding: 5rem 0;
+  }
+  .search-panel{
+    margin-top: 2rem;
+    padding: 1.5rem;
+    background-color: #f5eedf;
+    border-radius: 4px;
+    box-shadow: 15px 15px 15px rgba(0, 0, 0, 0.15);
   }
 </style>
