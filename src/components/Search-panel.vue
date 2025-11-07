88f29e78b39357bcb0cd0cf3f7482b24bd654353
Aziz Shoimqulov <azizshoimqulov74@gmail.com>
2025-11-07 11:58:50 +0500

    fdg

--- src/components/Search-panel.vue
+++ src/components/Search-panel.vue
@@ -14,9 +14,11 @@ export default {}
 <style scoped>
 .search_input{
     width: 100%;
-    margin-top: 1.5rem;
+    margin-top: 1rem;
+    border: 1px solid #000;
+    background-color: rgb(255, 255, 255);
     padding: 1rem;
-    border-radius: 8px;
+    border-radius: 4px;
     box-shadow: 15px 15px 15px rgba(0, 0, 0, 0.15);
 }
 </style>    
