ef95cdf77e4e456ea35e204f42c10cca024c7ac5
Aziz Shoimqulov <azizshoimqulov74@gmail.com>
2025-11-07 12:02:52 +0500

    gddg

--- src/components/app-filter.vue
+++ src/components/app-filter.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>
-        <h1>App Filter</h1>
+    <div class="app-filter">
+        <h1>Barcha kinolar</h1>
+        <h1>Mashhur kinolar</h1>
+        <h1>Eng ko'p kurilgan kinolar</h1>
     </div>
 </template>
 
@@ -10,5 +12,14 @@ export default {}
 
 
 <style scoped>
-    
+    .app-filter{
+        display: flex;
+        margin-top: 1.5rem;
+    }
+    .app-filter h1{
+        font-size: 1.2rem;
+        padding: 0.5rem 1rem;
+        border: 1px solid black;
+        cursor: pointer;
+    }
 </style>
